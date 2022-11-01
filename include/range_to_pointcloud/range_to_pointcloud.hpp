2dbe69cc11781939c2ba41cb55530ed794180441
mpheng666 <mpheng96@gmail.com>
2022-11-01 15:56:45 +0800

    Add sonar subs and pc pub

--- include/range_to_pointcloud/range_to_pointcloud.hpp
+++ include/range_to_pointcloud/range_to_pointcloud.hpp
@@ -1,14 +1,16 @@
 #ifndef RANGE_TO_POINTCLOUD_HPP_
 #define RANGE_TO_POINTCLOUD_HPP_
 
-#include <array>
-#include <geometry_msgs/Point.h>
 #include <ros/ros.h>
-#include <sensor_msgs/LaserScan.h>
 #include <sensor_msgs/Range.h>
-#include <std_msgs/Float64MultiArray.h>
+
 #include <string>
 #include <vector>
+
+#include "pcl_ros/point_cloud.h"
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
 
 #include "tf/transform_datatypes.h"
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
@@ -16,32 +18,46 @@
 #include "tf2_ros/transform_listener.h"
 #include <tf/transform_listener.h>
 
-#include "laser_geometry/laser_geometry.h"
 #include <sensor_msgs/PointCloud2.h>
 
 #include <boost/range/adaptors.hpp>
-
 namespace range_to_pointcloud {
-
     class RangeCallBack {
     public:
-        RangeCallBack(int index);
-        void operator()(const sensor_msgs::Range::ConstPtr& msg);
+        RangeCallBack(int index)
+            : index_(index)
+        {
+            tf2_ros::Buffer tfBuffer;
+            tf2_ros::TransformListener tfListener(tfBuffer);
+            std::string frame = "sonar_" + std::to_string(index_) + "_link";
 
-        double getUntransformedData();
-        std::array<double, 2> getTransformedData();
+            try {
+                transformStamped_baselink_sonar_ = tfBuffer.lookupTransform(
+                "base_link", frame, ros::Time(0), ros::Duration(1.0));
+            }
+            catch (tf2::TransformException& ex) {
+                ROS_WARN("%s", ex.what());
+            }
+        }
+
+        void operator()(const sensor_msgs::Range::ConstPtr& msg) { range_ = *msg; }
+
+        sensor_msgs::Range getRange() const { return range_; }
+
+        geometry_msgs::TransformStamped getTF() const
+        {
+            return transformStamped_baselink_sonar_;
+        }
 
     private:
         int index_;
-        double untransformed_range_data_{0.0};
-        std::array<double, 2> transformed_range_data_{0.0, 0.0};
-
+        sensor_msgs::Range range_;
         geometry_msgs::TransformStamped transformStamped_baselink_sonar_;
     };
 
     class RangeToPointCloud {
     public:
-        RangeToPointCloud(ros::NodeHandle& nh);
+        RangeToPointCloud(ros::NodeHandle& nh, int num);
         void start();
 
     private:
@@ -52,22 +68,12 @@ namespace range_to_pointcloud {
         ros::NodeHandle nh_;
         ros::NodeHandle nh_g_;
         ros::Timer pub_timer_;
-        ros::Publisher multi_sonars_pub_without_tf;
-        ros::Publisher multi_sonars_pub_with_tf;
-        ros::Subscriber scan_sub_;
-        ros::Publisher pc_pub_;
-
-        std::vector<RangeCallBack> range_callbacks_;
+        ros::Publisher pointcloud_pub_;
         std::vector<ros::Subscriber> range_sensors_subs_;
+        std::vector<RangeCallBack> range_callbacks_;
 
-        std::vector<double> utf_sonars_;
-        std::vector<std::array<double, 2>> tf_sonars_;
-
-        laser_geometry::LaserProjection projector_;
-        tf::TransformListener tflistener_;
-
-        void multiSonarsPub(const ros::TimerEvent&);
-        void scanCallBack(const sensor_msgs::LaserScan::ConstPtr& msg);
+        void timerPubCallBack(const ros::TimerEvent& e);
+        void populateRangeToPC(const sensor_msgs::Range& range_msg);
     };
 } // namespace range_to_pointcloud
 
--- node/range_to_pointcloud_node.cpp
+++ node/range_to_pointcloud_node.cpp
@@ -6,7 +6,7 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "range_to_pointcloud_node");
     ros::NodeHandle nh("~");
 
-    range_to_pointcloud::RangeToPointCloud range_to_pointcloud(nh);
+    range_to_pointcloud::RangeToPointCloud range_to_pointcloud(nh, 8);
     range_to_pointcloud.start();
 
     return 0;
